ad7a22d5d9eb4179e3a307bc315f9b7df08e6e22
현경 <nhk1657@kpu.ac.kr>
2023-06-12 23:55:11 +0900

    feat: 데이터 불러오기
    데이터프레임 형식으로 공공데이터 불러오기 (오류는 링크 수정으로 해결)

--- analysis.py
+++ analysis.py
@@ -1,19 +1,33 @@
 import requests
 import pprint
 import json
+from bs4 import BeautifulSoup
+import pandas as pd
 
 # 인증키
 encodingKey = "e%2BhnMTtUR6KCEeT0JaUw91rq9kQKnCDlWy5kQug3YNpYGx0gHDpQN6dMtNV7IfFt9irWjAbV60gDoq827vCPgQ%3D%3D"
 decodingKey = "e+hnMTtUR6KCEeT0JaUw91rq9kQKnCDlWy5kQug3YNpYGx0gHDpQN6dMtNV7IfFt9irWjAbV60gDoq827vCPgQ=="
 
 # URL
-url = "http://api.odcloud.kr/api/15083033/v1/uddi:5926b965-2a6b-498f-9757-8eb34949e0fc"
+url = "http://api.odcloud.kr/api/15083033/v1/uddi:bb89aa82-09a4-4d4d-91d9-9d6681bb09a7"
 params = {'serviceKey': decodingKey, 'pageNo': 1, 'numOfRows': 10}
 
 response = requests.get(url, params=params)
-contents = response.text
+# contents = response.text
 
-pp = pprint.PrettyPrinter(indent=4)
-print(pp.pprint(contents))
+# 깔끔한 출력
+# pp = pprint.PrettyPrinter(indent=4)
+# print(pp.pprint(contents)) 
 
-# 미완성, NONE으로 불러오기되는 오류 해결할 것
+# 예쁘게 출력하기
+
+soup = BeautifulSoup(response.text, "html.parser")
+
+list = []
+for item in soup.find_all("item"):
+    overcrowding = item.find("과밀지수").text
+    name = item.find("상권명칭").text
+    list.append([overcrowding, name])
+
+city_df = pd.DataFrame(list, columns= ["overcrowding", "name"])
+city_df.head()

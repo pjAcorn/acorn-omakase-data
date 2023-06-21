3741f1c215117d9ed02e922eddb5cf1566164fd5
현경 <nhk1657@kpu.ac.kr>
2023-06-21 13:53:56 +0900

    feat: 서버 최종 구축
    - 스프링부트 서버와 데이터 주고받기 구현
    - POST 요청의 매개 변수로 지역과 업종을 받고, 응답으로 결과 JSON 객체를 보냄

--- appController.py
+++ appController.py
@@ -1,19 +1,36 @@
 from flask import Flask, request, json, jsonify
-from flask_cors import CORS
+import csv
+import pandas as pd
 
 PORT_NUM = 8000
 
 app = Flask(__name__)
 
-@app.route("/test", methods=['POST'])
-def test():
-    params = request.get_json()
-    print("받은 데이터 ", params)
+def analyzeData(recievedData):
+    CSVDf = pd.read_csv("./years.csv")
 
-    response = {
-        "result": "OK"
-    }
-    
+    addressSido = recievedData['addressSido']
+    addressSigungu = recievedData['addressSigungu']
+    cateLData = recievedData['cateLData']
+    cateAData = recievedData['cateAData']
+    cateMData = recievedData['cateMData']
+
+    resultDf = CSVDf[(CSVDf['시도'] == addressSido) & (CSVDf['시군구'] == addressSigungu) &
+        (CSVDf['업종대분류'] == cateLData) & (CSVDf['분류'] == cateAData) & (CSVDf['업종중분류'] == cateMData)]
+    resultDf.drop({"시도", "시군구", "업종대분류", "분류", "업종중분류"}, axis=1, inplace=True)
+
+    responseData = pd.Series.to_json(resultDf, force_ascii=False)
+    print(responseData)
+
+    return responseData 
+
+
+@app.route("/analyze", methods=['POST'])
+def receiveData():
+    receivedData = request.get_json()
+    print("받은 데이터 ", receivedData)
+
+    response = analyzeData(receivedData)
     return jsonify(response)
 
 if __name__ == "__main__":
